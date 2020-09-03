ff2b31211867a955177ea9470c6c1106cb830264
andrei <losev_adrei@bk.ru>
2020-09-03 19:27:55 +0300

    reademe init

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -1,9 +1,7 @@
-const product = require('./product');
 const users = require('./users');
 const message = require('./message');
 
 module.exports = {
-	product,
 	users,
 	message,
 };
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,29 +1,23 @@
-const products = require('../app/controllers/products');
 const auth = require('../app/controllers/auth');
 const user = require('../app/controllers/users');
 const message = require('../app/controllers/message');
-//const authMiddleware = require('../app/middleware/auth');
+const authMiddleware = require('../app/middleware/auth');
 
 
 module.exports = (app) => {
-	// product
-	app.get('/products', /*authMiddleware,*/ products.getAll);
-	app.post('/products', /*authMiddleware,*/ products.create);
-	app.put('/products/:id', /*authMiddleware,*/ products.update);
-	app.delete('/products/:id', /*authMiddleware,*/ products.remove);
-
 	// auth
 	app.post('/signin', auth.signIn);
+
 	// users
-	app.get('/user', user.getAllUsers);
-	app.post('/user', user.createUser);
-	app.put('/user/:id', user.userUpdate);
-	app.delete('/user/:name', user.removeUser);
-	app.get('/find/:searchParam', user.getFindUsers);
+	app.get('/user', authMiddleware, user.getAllUsers);
+	app.post('/user', authMiddleware, user.createUser);
+	app.put('/user/:id', authMiddleware, user.userUpdate);
+	app.delete('/user/:name', authMiddleware, user.removeUser);
+	app.get('/find/:searchParam', authMiddleware, user.getFindUsers);
 
 	// message
-	app.get('/message', message.getAll)
-	app.post('/message', message.createMessage);
-	app.delete('/message/:id', message.removeMessage);
-	app.put('/message', message.getMessage)
+	app.get('/message', authMiddleware, message.getAll)
+	app.post('/message', authMiddleware, message.createMessage);
+	app.delete('/message/:id', authMiddleware, message.removeMessage);
+	app.put('/message', authMiddleware, message.getMessage)
 };
